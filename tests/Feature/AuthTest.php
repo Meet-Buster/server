afa7078932328e929913da9b2f300371693b53ef
Belal-2000 <BelalIbrahim080@gmail.com>
2024-08-24 21:13:40 +0300

    Test: Adding login tests

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -11,7 +11,12 @@ class AuthTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_it_return_valid_json_with_token(): void
+    protected function createUser($userData): User
+    {
+        return User::factory(1)->createOne($userData);
+    }
+
+    public function test_it_return_valid_json_with_token_in_register_request(): void
     {
         $payload = [
             'name' => 'test',
@@ -27,7 +32,7 @@ class AuthTest extends TestCase
             ->assertJsonStructure(['user', 'token']);
     }
 
-    public function test_it_fail_when_receive_invalid_data(): void
+    public function test_it_fail_when_receive_invalid_data_in_register_request(): void
     {
         $payload = [
             'email' => 'test',
@@ -39,6 +44,47 @@ class AuthTest extends TestCase
 
         $response
             ->assertSessionHasErrors(['name', 'email', 'password']);
+    }
+
+    public function test_it_return_valid_json_with_token_in_login_request(): void
+    {
+        $userData = [
+            'email' => 'test@test.com',
+            'password' => 'password'
+        ];
+
+        $this->createUser($userData);
+
+        $payload = [
+            'email' => $userData['email'],
+            'password' => $userData['password'],
+        ];
+
+        $response = $this->post('/api/auth/login', $payload);
+
+        $response
+            ->assertOk()
+            ->assertJsonStructure(['user', 'token']);
+    }
+
+    public function test_it_fail_when_receive_invalid_data_in_login_request(): void
+    {
+        $userData = [
+            'email' => 'test@test.com',
+            'password' => 'password'
+        ];
+
+        $this->createUser($userData);
+
+        $payload = [
+            'email' => $userData['email'],
+            'password' => 'pas',
+        ];
+
+        $response = $this->post('/api/auth/register', $payload);
+
+        $response
+            ->assertSessionHasErrors(['email']);
     }
 
     public function test_it_can_logout_user(): void
